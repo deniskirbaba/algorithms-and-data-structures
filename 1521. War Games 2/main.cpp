27f3f35fdfc10c19d1d0636e0f4901f78b195231
pivp <kirbabadenis@gmail.com>
2022-04-21 22:15:36 +0300

    added war games task

--- 1521. War Games 2/main.cpp
+++ 1521. War Games 2/main.cpp
@@ -1,6 +1,23 @@
+#include <vector>
 #include <iostream>
 
+using std::cin, std::cout, std::vector;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    uint32_t n, k;
+    cin >> n >> k;
+
+    vector<uint32_t> soldiers(n);
+
+    for (uint32_t i = 0; i < n; i++) soldiers[i] = i + 1;
+
+    uint32_t outIndex = k - 1;
+    for (uint32_t i = 0; i < n; i++) {
+        cout << soldiers[outIndex] << ' ';
+        soldiers.erase(soldiers.begin() + outIndex);
+        if (!soldiers.empty()) outIndex = ((outIndex + (k - 1)) % soldiers.size());
+    }
+
     return 0;
 }
